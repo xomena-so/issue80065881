6d102533486da2339a2430d3c1f340fef62c0964
xomena-so <geo.emea.sbt@gmail.com>
2018-05-29 15:55:28 +0200

    Initial Commit

---
+++ issue80065881/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  issue80065881
+//
+//  Created by Google Training 2 on 29/05/2018.
+//  Copyright © 2018 Google Trainining. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
