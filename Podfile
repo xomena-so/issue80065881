551c884d71dc71cbaa8cdc5217e7ffa112f33397
xomena-so <geo.emea.sbt@gmail.com>
2018-05-30 16:17:50 +0200

    sample fit to polyline

---
+++ Podfile
@@ -0,0 +1,4 @@
+source 'https://github.com/CocoaPods/Specs.git'
+target 'issue80065881' do
+  pod 'GoogleMaps'
+end
--- issue80065881/ViewController.swift
+++ issue80065881/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import GoogleMaps
 
 class ViewController: UIViewController {
 
@@ -20,6 +21,28 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func loadView() {
+        // Create a GMSCameraPosition that tells the map to display the
+        // coordinate -33.86,151.20 at zoom level 6.
+        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 18.0)
+        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        view = mapView
+        
+        //let encPoly = "lysmE_}zy[E??SIaJAiBBMDUGGgAkAc@n@s@s@"
+        //let poly = GMSPolyline.init(path: GMSPath.init(fromEncodedPath: encPoly))
+        let path = GMSMutablePath()
+        path.add(CLLocationCoordinate2D(latitude: -33.85, longitude: 151.20))
+        path.add(CLLocationCoordinate2D(latitude: -33.70, longitude: 151.40))
+        path.add(CLLocationCoordinate2D(latitude: -33.73, longitude: 151.41))
+        let poly = GMSPolyline(path: path)
+        poly.map = mapView
+        
+        let p = poly.path!
+        let b = GMSCoordinateBounds(path: p)
+        let update = GMSCameraUpdate.fit(b, withPadding: CGFloat.init())
+        
+        mapView.moveCamera(update)
+    }
 
 }
 
